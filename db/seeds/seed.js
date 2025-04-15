d1cb080ca5a978cb6e97bfa9ac72c56cbbad7acf
David Potter <davidgeorgepotter@protonmail.com>
2025-04-15 16:32:03 +0100

    seed.js passing all tests

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,6 +1,104 @@
-const db = require("../connection")
+const db = require("../connection");
+const format = require("pg-format");
+const { convertTimestampToDate } = require("./utils");
+const comments = require("../data/test-data/comments");
 
 const seed = ({ topicData, userData, articleData, commentData }) => {
-  return db.query(); //<< write your first query in here.
+  return db
+    .query("DROP TABLE IF EXISTS comments;")
+    .then(() => {
+      db.query("DROP TABLE IF EXISTS articles;");
+    })
+    .then(() => {
+      return db.query("DROP TABLE IF EXISTS users;");
+    })
+    .then(() => {
+      return db.query("DROP TABLE IF EXISTS topics;");
+    })
+    .then(() => {
+      return db.query(
+        "CREATE TABLE topics (slug VARCHAR(100) PRIMARY KEY, description VARCHAR(300), img_url VARCHAR(1000));"
+      );
+    })
+    .then(() => {
+      return db.query(
+        "CREATE TABLE users (username VARCHAR(150) PRIMARY KEY, name VARCHAR(150), avatar_url VARCHAR(1000));"
+      );
+    })
+    .then(() => {
+      return db.query(
+        "CREATE TABLE articles (article_id SERIAL PRIMARY KEY, title VARCHAR(300), topic VARCHAR(200) REFERENCES topics(slug), author VARCHAR(200) REFERENCES users(username), body TEXT, created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, votes INT DEFAULT 0, article_img_url VARCHAR(1000));"
+      );
+    })
+    .then(() => {
+      return db.query(
+        `CREATE TABLE comments 
+        (comment_id SERIAL PRIMARY KEY, 
+        article_id INT REFERENCES articles(article_id), 
+        body TEXT, 
+        votes INT DEFAULT 0, 
+        author VARCHAR(200) REFERENCES users(username),
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);`
+      );
+    })
+    .then(() => {
+      //format the data
+      const formattedTopicData = topicData.map((topic) => {
+        return [topic.description, topic.slug, topic.img_url];
+      });
+      //pass it to pg format and get a string
+      const insertIntoTopicQuery = format(
+        `INSERT INTO topics VALUES %L`,
+        formattedTopicData
+      );
+      //pass the string to db.query
+      return db.query(insertIntoTopicQuery);
+    })
+    .then(() => {
+      const formattedUserData = userData.map((user) => {
+        return [user.username, user.name, user.avatar_url];
+      });
+      const insertIntoUserQuery = format(
+        `INSERT INTO users VALUES %L`,
+        formattedUserData
+      );
+      return db.query(insertIntoUserQuery);
+    })
+    .then(() => {
+      const articleDataConvertedTimestamp = convertTimestampToDate(articleData); //uses util to convert created_at to correct format - leaves other values as they were
+      const formattedArticleData = articleData.map((article) => {
+        return [
+          articleDataConvertedTimestamp.title,
+          articleDataConvertedTimestamp.topic,
+          articleDataConvertedTimestamp.author,
+          articleDataConvertedTimestamp.body,
+          articleDataConvertedTimestamp.created_at,
+          articleDataConvertedTimestamp.votes,
+          articleDataConvertedTimestamp.article_img_url,
+        ];
+      });
+      const insertIntoArticleQuery = format(
+        `INSERT INTO articles (title, topic, author, body, created_at, votes, article_img_url) VALUES %L`,
+        formattedArticleData
+      );
+      return db.query(insertIntoArticleQuery);
+    })
+    .then(() => {
+      const formattedCommentData = comments.map((comment) => {
+        const convertedComment = convertTimestampToDate(comment);
+        return [
+          convertedComment.body,
+          convertedComment.votes,
+          convertedComment.author,
+          convertedComment.created_at,
+        ];
+      });
+      const insertIntoCommentQuery = format(
+        `INSERT INTO comments (body, votes, author, created_at) VALUES %L`,
+        formattedCommentData
+      );
+      return db.query(insertIntoCommentQuery);
+    });
 };
+
 module.exports = seed;
