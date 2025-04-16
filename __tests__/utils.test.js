647908035ca36da5c0f51fba5e4d753cbef48f03
David Potter <davidgeorgepotter@protonmail.com>
2025-04-16 10:19:21 +0100

    createRef util and accompanying tests

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -1,6 +1,4 @@
-const {
-  convertTimestampToDate
-} = require("../db/seeds/utils");
+const { convertTimestampToDate, createRef } = require("../db/seeds/utils");
 
 describe("convertTimestampToDate", () => {
   test("returns a new object", () => {
@@ -38,3 +36,58 @@ describe("convertTimestampToDate", () => {
   });
 });
 
+describe("createRef", () => {
+  test("should return an empty object when passed an empty array", () => {
+    const input = [];
+    const result = createRef(input);
+    expect(result).toEqual({});
+  });
+  test("should return a ref object with a single key and value when passed an array containing a single article object", () => {
+    const input = [
+      {
+        article_id: 3,
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: 1594329060000,
+        votes: 100,
+        article_img_url:
+          "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+      },
+    ];
+    const result = createRef(input);
+    expect(result).toEqual({ "Living in the shadow of a great man": 3 });
+  });
+  test("handles an array containing multiple article objects", () => {
+    const input = [
+      {
+        article_id: 5,
+        title: "Sony Vaio; or, The Laptop",
+        topic: "mitch",
+        author: "icellusedkars",
+        body: "I find this existence extremely challenging.",
+        created_at: 1602828180000,
+        votes: 0,
+        article_img_url:
+          "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+      },
+      {
+        article_id: 8,
+        title: "Eight pug gifs that remind me of mitch",
+        topic: "mitch",
+        author: "icellusedkars",
+        body: "some gifs",
+        created_at: 1604394720000,
+        votes: 0,
+        article_img_url:
+          "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+      },
+    ];
+    const result = createRef(input);
+    expect(result).toEqual({
+      "Sony Vaio; or, The Laptop": 5,
+      "Eight pug gifs that remind me of mitch": 8,
+    });
+  });
+});
--- db/seeds/utils.js
+++ db/seeds/utils.js
@@ -1,6 +1,20 @@
 const db = require("../../db/connection");
+const articles = require("../data/test-data/articles");
 
 exports.convertTimestampToDate = ({ created_at, ...otherProperties }) => {
   if (!created_at) return { ...otherProperties };
   return { created_at: new Date(created_at), ...otherProperties };
+};
+
+exports.createRef = (articlesArr) => {
+  //takes an array of article objects, gives title:id lookup
+  if (articlesArr.length === 0) {
+    return {};
+  } else {
+    const lookupObj = {};
+    articlesArr.forEach((article) => {
+      lookupObj[article.title] = article.article_id;
+    });
+    return lookupObj;
+  }
 };
