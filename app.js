781bec5d1d470fd1ee10ff01e741a1ca799f007d
David Potter <davidgeorgepotter@protonmail.com>
2025-04-28 14:38:02 +0100

    Update app.js to include separate error handling required in from errors controller. Added 500 catch all

--- app.js
+++ app.js
@@ -10,12 +10,17 @@ const {
   getArticleByArticleId,
   getAllArticles,
 } = require("./app/controllers/articles.controllers");
+const {
+  handlePSQLErrors,
+  handleCustomErrors,
+  catchAllErrors,
+} = require("./app/controllers/errors.controllers");
 
 app.get("/api", getApi);
 
 app.get("/api/topics", getAllTopics);
 
-app.get("api/articles", getAllArticles);
+app.get("/api/articles", getAllArticles);
 
 app.get("/api/articles/:article_id", getArticleByArticleId);
 
@@ -23,14 +28,8 @@ app.all("/*splat", (req, res, next) => {
   res.status(404).send({ msg: "Invalid URL!" });
 });
 
-app.use((err, req, res, next) => {
-  // console.log(err.code);
-  if (err.code === "22P02") {
-    res.status(400).send({ msg: "Invalid request!" });
-  }
-  if (err.msg && err.status) {
-    res.status(err.status).send({ msg: err.msg });
-  }
-});
+app.use(handlePSQLErrors);
+app.use(handleCustomErrors);
+app.use(catchAllErrors);
 
 module.exports = app;
