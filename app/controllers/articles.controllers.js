889399dd2751a9803f48281a7efe91aa64a72221
David Potter <davidgeorgepotter@protonmail.com>
2025-04-30 10:01:03 +0100

    Move conditional logic from articles.models to articles.controllers to handle rejects earlier

--- app/controllers/articles.controllers.js
+++ app/controllers/articles.controllers.js
@@ -14,8 +14,76 @@ const getArticleByArticleId = (req, res, next) => {
 };
 
 const getAllArticles = (req, res, next) => {
-  const { sort_by, order } = req.query;
-  return selectAllArticles(sort_by, order)
+  const { sort_by, order, topic } = req.query;
+  ////////
+  //check for topic existing
+  const selectTopicByTopic = (topicName) => {
+    return db
+      .query(`SELECT * FROM topics WHERE slug = $1`, [topicName])
+      .then(({ rows }) => {
+        if (rows.length === 0) {
+          Promise.reject();
+        }
+      });
+  };
+  ////////
+  // const pendingSelectAllArticles = selectAllArticles(sort_by, order, topic);
+  // const pendingSelectTopicByTopic = selectTopicByTopic(topic);
+  ///////
+
+  //moving model logic into controller
+  let queryStr = `WITH mainCommentsInfo AS
+          (SELECT author, title, article_id, topic, created_at, votes, article_img_url
+          FROM articles),
+
+          commentCountInfo AS
+          (SELECT article_id, COUNT(comment_id)::INT
+          AS comment_count
+          FROM comments
+          GROUP BY article_id)
+
+          SELECT mainCommentsInfo.author,
+          mainCommentsInfo.title, mainCommentsInfo.article_id, mainCommentsInfo.topic, mainCommentsInfo.created_at, mainCommentsInfo.votes, mainCommentsInfo.article_img_url,
+          COALESCE(commentCountInfo.comment_count, 0) AS comment_count
+          FROM mainCommentsInfo
+
+          LEFT JOIN commentCountInfo 
+          ON mainCommentsInfo.article_id = commentCountInfo.article_id`;
+  const sortByGreenlist = ["created_at", "votes", "topic", "author", "title"];
+  const orderGreenlist = ["asc", "desc"];
+  const sortByRedlist = ["body", "article_img_url"];
+  //sort_by handling
+  if (!sort_by) {
+    queryStr += ` ORDER BY created_at`;
+  }
+  if (sort_by) {
+    if (sortByGreenlist.includes(sort_by)) {
+      queryStr += ` ORDER BY ${sort_by}`;
+    }
+    if (sortByRedlist.includes(sort_by)) {
+      return Promise.reject({ status: 401, msg: "Unauthorised request!" });
+    }
+  }
+  //400 rejects
+  if (
+    (sort_by && !sortByGreenlist.includes(sort_by)) ||
+    sort_by === "" ||
+    (order && !orderGreenlist.includes(order.toLowerCase())) ||
+    order === ""
+  ) {
+    return Promise.reject({ status: 400, msg: "Invalid request!" });
+  }
+  //order handling
+  if (!order) {
+    queryStr += ` DESC`;
+  }
+  if (order && orderGreenlist.includes(order.toLowerCase())) {
+    queryStr += ` ${order}`;
+  }
+  ///////////end of model logic above
+
+  // Promise.all([pendingSelectAllArticles, pendingSelectTopicByTopic])
+  return selectAllArticles(queryStr)
     .then((articles) => {
       res.status(200).send({ articles });
     })
--- app/models/articles.models.js
+++ app/models/articles.models.js
@@ -12,92 +12,11 @@ const selectArticleByArticleId = (articleId) => {
     });
 };
 
-const selectAllArticles = (sort_by, order, topic) => {
-  let queryStr = `WITH mainCommentsInfo AS
-          (SELECT author, title, article_id, topic, created_at, votes, article_img_url
-          FROM articles),
-
-          commentCountInfo AS
-          (SELECT article_id, COUNT(comment_id)::INT
-          AS comment_count
-          FROM comments
-          GROUP BY article_id)
-
-          SELECT mainCommentsInfo.author,
-          mainCommentsInfo.title, mainCommentsInfo.article_id, mainCommentsInfo.topic, mainCommentsInfo.created_at, mainCommentsInfo.votes, mainCommentsInfo.article_img_url,
-          COALESCE(commentCountInfo.comment_count, 0) AS comment_count
-          FROM mainCommentsInfo
-
-          LEFT JOIN commentCountInfo 
-          ON mainCommentsInfo.article_id = commentCountInfo.article_id`;
-  const sortByGreenlist = ["created_at", "votes", "topic", "author", "title"];
-  const orderGreenlist = ["asc", "desc"];
-  const sortByRedlist = ["body", "article_img_url"];
-
-  //topic query brought in here, need to do promise to check if it exists
-
-  //sort_by handling
-  if (!sort_by) {
-    queryStr += ` ORDER BY created_at`;
-  }
-  if (sort_by) {
-    if (sortByGreenlist.includes(sort_by)) {
-      queryStr += ` ORDER BY ${sort_by}`;
-    }
-    if (sortByRedlist.includes(sort_by)) {
-      return Promise.reject({ status: 401, msg: "Unauthorised request!" });
-    }
-  }
-  //400 reject
-  if (
-    (sort_by && !sortByGreenlist.includes(sort_by)) ||
-    sort_by === "" ||
-    (order && !orderGreenlist.includes(order.toLowerCase())) ||
-    order === ""
-  ) {
-    return Promise.reject({ status: 400, msg: "Invalid request!" });
-  }
-  //order handling
-  if (!order) {
-    queryStr += ` DESC`;
-  }
-  if (order && orderGreenlist.includes(order.toLowerCase())) {
-    queryStr += ` ${order}`;
-  }
-
+const selectAllArticles = (queryStr) => {
   return db.query(queryStr).then(({ rows: articles }) => {
     return articles;
   });
 };
-
-// const selectAllArticles = (sort_by, order) => {
-
-//   return db
-//     .query(
-//       `WITH mainCommentsInfo AS
-//           (SELECT author, title, article_id, topic, created_at, votes, article_img_url
-//           FROM articles),
-
-//           commentCountInfo AS
-//           (SELECT article_id, COUNT(comment_id)::INT
-//           AS comment_count
-//           FROM comments
-//           GROUP BY article_id)
-
-//           SELECT mainCommentsInfo.author,
-//           mainCommentsInfo.title, mainCommentsInfo.article_id, mainCommentsInfo.topic, mainCommentsInfo.created_at, mainCommentsInfo.votes, mainCommentsInfo.article_img_url,
-//           COALESCE(commentCountInfo.comment_count, 0) AS comment_count
-//           FROM mainCommentsInfo
-
-//           LEFT JOIN commentCountInfo
-//           ON mainCommentsInfo.article_id = commentCountInfo.article_id
-//           ORDER BY created_at DESC
-//             `
-//     )
-//     .then(({ rows }) => {
-//       return rows;
-//     });
-// };
 
 const updateVotesOnArticle = (voteIncrement, articleId) => {
   return db
