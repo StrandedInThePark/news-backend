46540ac9fd63ad0b843637785141ba0d635607f3
David Potter <davidgeorgepotter@protonmail.com>
2025-04-30 14:42:19 +0100

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -2,15 +2,14 @@ const { Pool } = require("pg");
 
 const ENV = process.env.NODE_ENV || "development";
 
+const config = {};
 require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
 
-const config = {};
-
+// const db = new Pool(config);
 if (ENV === "production") {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
 }
-const db = new Pool(config);
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("No PGDATABASE or DATABASE_URL configured");
@@ -18,5 +17,4 @@ if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   console.log(`Connected to ${process.env.PGDATABASE}`);
 }
 
-// module.exports = db;
-module.exports = db;
+module.exports = new Pool(config);
